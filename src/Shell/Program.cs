d6da559ae23a2a83a7e2c89d44c387b4bf900fa6
Chris Anders <filmee24@gmail.com>
2025-12-17 22:10:04 +0100

    feature: Add Velopack

--- src/Shell/Program.cs
+++ src/Shell/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Velopack;
+using Velopack.Sources;
+
+namespace BrineAndCoin;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        VelopackApp
+                   .Build()
+#if WINDOWS
+                       .OnAfterInstallFastCallback(v => new Shortcuts().CreateShortcutForThisExe(ShortcutLocation.Desktop))
+#endif
+                   .Run();
+
+#if RELEASE
+                   CheckForUpdatesAsync();
+#endif
+    }
+
+    private static async void CheckForUpdatesAsync()
+    {
+        var mgr = new UpdateManager(new GithubSource("https://github.com/Portraits-in-Brick-and-Time/Brine-and-Coin", null, false));
+
+        var newVersion = await mgr.CheckForUpdatesAsync();
+        if (newVersion == null)
+            return;
+
+        //ToDo: add ui for downloading updates
+        await mgr.DownloadUpdatesAsync(newVersion);
+
+        mgr.ApplyUpdatesAndRestart(newVersion);
+    }
+}
