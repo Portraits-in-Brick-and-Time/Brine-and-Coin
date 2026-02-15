c9c15e1371f171402d0a03a1db767c791e6c592a
Chris Anders <filmee24@gmail.com>
2026-02-15 20:05:20 +0100

    feat: Add Parser

---
+++ src/ObjectModel/Language/LanguageParser.cs
@@ -0,0 +1,31 @@
+using Silverfly;
+using Silverfly.Lexing.IgnoreMatcher.Comments;
+using Silverfly.Parselets;
+
+namespace ObjectModel.Language;
+
+public class LanguageParser : Parser
+{
+    protected override void InitLexer(LexerConfig lexer)
+    {
+        lexer.AddKeywords("let", "if", "then", "else", "enum", "func");
+
+        lexer.IgnoreWhitespace();
+        lexer.MatchNumber(allowHex: false, allowBin: false);
+        lexer.MatchString("\"", "\"");
+
+        lexer.Ignore(new SingleLineCommentIgnoreMatcher("//"));
+        lexer.Ignore(new MultiLineCommentIgnoreMatcher("/*", "*/"));
+    }
+
+    protected override void InitParser(ParserDefinition parser)
+    {
+        parser.AddArithmeticOperators();
+        parser.AddCommonLiterals();
+
+        parser.Register("(", new CallParselet(parser.PrecedenceLevels.GetPrecedence("Call")));
+        parser.Register(PredefinedSymbols.Name, new NameParselet());
+
+        parser.Block(PredefinedSymbols.SOF, PredefinedSymbols.EOF, PredefinedSymbols.EOL);
+    }
+}
